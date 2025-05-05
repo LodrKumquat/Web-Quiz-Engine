72264c12b1a8386d7ed1150b2a3be04b24814a01
LordKumquat <7691262@gmail.com>
2025-05-05 17:38:14 +0200

    Improve user email validation

--- src/engine/dto/QuizUserRegistrationRequest.java
+++ src/engine/dto/QuizUserRegistrationRequest.java
@@ -1,6 +1,9 @@
 package engine.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record QuizUserRegistrationRequest(@Email String email, @Size(min = 5) String password) {}
+public record QuizUserRegistrationRequest(
+        @Email(regexp = "^\\w+@\\w+\\.[A-Za-z]{2,}$") String email,
+        @NotBlank @Size(min = 5) String password) {}
